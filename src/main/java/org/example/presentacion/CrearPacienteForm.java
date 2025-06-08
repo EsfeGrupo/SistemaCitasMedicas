1ce516ac15af31d4725c64f067501076e0bf8c91
Brayan Eduardo Tepas Linares <2675200@edu.sv>
2025-06-08 14:53:19 -0600

    se creo formulario para crear pacientes

---
+++ src/main/java/org/example/presentacion/CrearPacienteForm.java
@@ -0,0 +1,18 @@
+package org.example.presentacion;
+
+import javax.swing.*;
+
+public class CrearPacienteForm {
+    private JPanel MainPanel;
+    private JTextField txtNombre;
+    private JTextField txtDireccion;
+    private JTextField txtTelefono;
+    private JTextField txtFecha;
+    private JComboBox comboBox1;
+    private JButton btnCrear;
+    private JButton btnCancelar;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
