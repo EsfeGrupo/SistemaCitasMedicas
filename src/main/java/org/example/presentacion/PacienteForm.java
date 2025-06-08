6d1a8b6e6fbf5a715af3fee209935046f97a158e
Brayan Eduardo Tepas Linares <2675200@edu.sv>
2025-06-07 20:53:31 -0600

    se creo la carpeta de imagenes y presentacion aparte se creo la main de pacientes

---
+++ src/main/java/org/example/presentacion/PacienteForm.java
@@ -0,0 +1,16 @@
+package org.example.presentacion;
+
+import javax.swing.*;
+
+public class PacienteForm {
+    private JPanel mainPanel;
+    private JPanel pnlTituloOperaciones;
+    private JPanel pnlTitulo;
+    private JPanel pnlOperaciones;
+    private JButton btnEliminar;
+    private JButton btnEditar;
+    private JButton btnCrear;
+    private JTable table1;
+    private JTextField txtBuscar;
+
+}
