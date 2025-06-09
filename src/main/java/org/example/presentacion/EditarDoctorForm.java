ad3f0ac29a1a837d39a710f084c6f4d84e409a29
Brayan Eduardo Tepas Linares <2675200@edu.sv>
2025-06-09 09:47:25 -0600

    se termino el crud de vistas de doctor

---
+++ src/main/java/org/example/presentacion/EditarDoctorForm.java
@@ -0,0 +1,13 @@
+package org.example.presentacion;
+
+import javax.swing.*;
+
+public class EditarDoctorForm {
+    private JPanel MainPanel;
+    private JTextField txtEspecialidad;
+    private JTextField txtNombre;
+    private JTextField txtExperiencia;
+    private JComboBox cboDisponibilidad;
+    private JButton btnCrear;
+    private JButton btnCancelar;
+}
