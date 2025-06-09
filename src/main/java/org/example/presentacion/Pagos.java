c1b9aaf45de8efa99b0546939e8a946cdce04c07
Cristian Daniel Colocho López <colochocristian@outlook.com>
2025-06-08 22:20:58 -0600

    Form pagos de citas

---
+++ src/main/java/org/example/presentacion/Pagos.java
@@ -0,0 +1,10 @@
+package org.example.presentacion;
+
+import javax.swing.*;
+
+public class Pagos {
+    private JLabel LPresentacion;
+    private JPanel JContenido;
+    private JTextField JNombre;
+    private JButton pagarButton;
+}
