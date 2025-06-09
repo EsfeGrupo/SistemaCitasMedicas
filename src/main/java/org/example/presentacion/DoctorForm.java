c322e488ebd49fc1114d8347e92274a3cee66bf6
Fran-mndz <shiro54828@gmail.com>
2025-06-09 16:51:32 -0600

    hago commit de algo q ni yo se pq lo hice en la mañana, luego vere que es jajaj

--- src/main/java/org/example/presentacion/DoctorForm.java
+++ src/main/java/org/example/presentacion/DoctorForm.java
@@ -1,8 +1,9 @@
 package org.example.presentacion;
 
 import javax.swing.*;
+import java.awt.*;
 
-public class DoctorForm {
+public class DoctorForm extends JDialog {
     private JPanel mainPanel;
     private JPanel pnlTituloOperaciones;
     private JPanel pnlOperaciones;
@@ -12,4 +13,15 @@ public class DoctorForm {
     private JTextField txtBuscar;
     private JPanel pnlTitulo;
     private JTable tabDetalles;
+
+    private MainForm mainForm; // Referencia a la ventana principal de la aplicación.
+
+    public DoctorForm(MainForm mainForm) {
+        this.mainForm = mainForm; // Inicializa la referencia a la ventana principal.
+        setContentPane(mainPanel); // Establece el panel principal como el contenido de este diálogo.
+        setModal(true); // Hace que este diálogo sea modal, lo que significa que bloquea la interacción con la ventana principal hasta que se cierre.
+        setTitle("Doctores"); // Establece el título de la ventana del diálogo.
+        pack(); // Ajusta el tamaño de la ventana para que todos sus componentes se muestren correctamente.
+        setLocationRelativeTo(mainForm); // Centra la ventana del diálogo relative a la ventana principal.
+    }
 }
