0c5eb41f56e53b2fd4753b33393e123c72dd3d65
Ariel Villegas <arielvillegasflores@gmail.com>
2025-06-09 09:52:38 -0600

    se hizo el login y la vista del CRUD de usuarios

---
+++ src/main/java/org/example/presentacion/Usersform.java
@@ -0,0 +1,15 @@
+package org.example.presentacion;
+
+import javax.swing.*;
+
+public class Usersform {
+    private JPanel mainPanel;
+    private JPanel pnlTituloOperaciones;
+    private JPanel pnlOperaciones;
+    private JButton btnEditar;
+    private JButton btnEliminar;
+    private JButton btnCrear;
+    private JTextField txtBuscar;
+    private JPanel pnlTitulo;
+    private JTable table1;
+}
