4f85d2f0f2e127e67a05a0a0eb95fcea50301f4d
Carlos Ariel Villegas Flores <2675200@edu.sv>
2025-06-16 09:42:15 -0600

    correciones login

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,15 +2,18 @@ package org.example;
 
 import org.example.presentacion.MainForm;
 import javax.swing.*;
+import org.example.presentacion.LoginForm;
 
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
+            new LoginForm().setVisible(true); // Este abre el login primero
+
             // Utiliza el hilo de despacho de eventos (Event Dispatch Thread - EDT) para asegurar
             // que todas las operaciones relacionadas con la interfaz gráfica de usuario (Swing)
             // se realicen de forma segura y sin bloqueos.
-            MainForm mainForm  = new MainForm(); // Crea una nueva instancia del formulario principal de la aplicación.
-            mainForm.setVisible(true); // Hace visible el formulario principal. Inicialmente podría estar vacío o tener una interfaz de carga.
-          });
+            // MainForm mainForm  = new MainForm(); // Crea una nueva instancia del formulario principal de la aplicación.
+            //  mainForm.setVisible(true); // Hace visible el formulario principal. Inicialmente podría estar vacío o tener una interfaz de carga.
+        });
     }
 }
